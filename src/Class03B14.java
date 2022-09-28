772707c9e4ecfa25b50b405f17e699ce11d0311d
busica777 <gotruck.312@gmail.com>
2022-09-27 19:04:40 -0500

    second commit

--- src/Class03B14.java
+++ src/Class03B14.java
@@ -3,7 +3,10 @@ public class Class03B14 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+System.out.println("hey  this is git class");
+		System.out.println("This is a change in my project");
+		
+		
 	}
 
 }
